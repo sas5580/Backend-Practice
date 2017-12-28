bb1fb0ed3ce54d42c57e1966eb5ab314ad3b8105
Saswata Gupta <gupta.saswata@gmail.com>
2017-12-28 10:47:35 -0500

    Added Data Accessor Object

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from DataAccessor import DataAccessor
 
 app = Flask(__name__)
 api = Api(app)
+dao = DataAccessor(app)
 
 class HelloWorld(Resource):
-    def get(self):
+    def get(self, op):
         return {'hello': 'world'}
 
-api.add_resource(HelloWorld, '/')
+api.add_resource(HelloWorld, '/<op>')
 
 if __name__ == '__main__':
     app.run(debug=True)
