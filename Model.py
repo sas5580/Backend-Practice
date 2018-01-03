d33d1b1df0ce8a9efdce43dc835583801ebeddcf
Saswata Gupta <gupta.saswata@gmail.com>
2018-01-03 18:13:54 -0500

    Added intialisers from dicts for Model classes

--- Model.py
+++ Model.py
@@ -1,4 +1,4 @@
-from typing import Iterable, List
+from typing import Iterable, List, Dict
 from copy import deepcopy
 
 DAYS = set(('Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'))
@@ -8,6 +8,10 @@ class Time:
         assert 0 <= minute < 60 and 0 <= hour < 24
         self.hour = hour
         self.minute = minute
+
+    @classmethod
+    def fromdict(cls, time_dict: Dict[str, int]):
+        return cls(time_dict['hour'], time_dict['minute'])
 
     def inSeconds(self):
         return self.hour*60 + self.minute
@@ -26,6 +30,15 @@ class Event:
         self.toTime = toTime
         self.description = description
 
+    @classmethod
+    def fromdict(cls, event_dict):
+        name = event_dict['name']
+        days = event_dict['days']
+        fromTime = Time.fromdict(event_dict['fromTime'])
+        toTime = Time.fromdict(event_dict['toTime'])
+        description = event_dict['description']
+        return cls(name, days, fromTime, toTime, description)
+
     def serialize(self):
         event_dict = deepcopy(vars(self))
         event_dict['fromTime'] = vars(event_dict['fromTime'])
@@ -36,6 +49,12 @@ class Schedule:
     def __init__(self, owner: str, events: List[Event]=[]):
         self.owner = owner
         self.events = events
+
+    @classmethod
+    def fromdict(cls, sched_dict):
+        owner = sched_dict['owner']
+        events = [Event.fromdict(e) for e in sched_dict['events']]
+        return cls(owner, events)
 
     def addEvent(self, event: Event):
         self.events.append(event)
@@ -55,6 +74,11 @@ class Schedule:
             event.toTime = newToTime
         if newDescription is not None:
             event.description = newDescription
+
+    def serialize(self):
+        sched_dict = {'owner': self.owner}
+        sched_dict['events'] = [e.serialize() for e in self.events]
+        return sched_dict
 
     def __getitem__(self, day: str):
         if day not in DAYS:
