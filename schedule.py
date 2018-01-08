4567f2b67c5198c18074d7932ccc8873e8668aab
Saswata Gupta <gupta.saswata@gmail.com>
2018-01-08 17:17:20 -0500

    Refactored Schedule model

--- schedule.py
+++ schedule.py
@@ -1,8 +1,12 @@
 from typing import Iterable
+from flask_restful import abort
 
+from scheduledao import ScheduleDAO
 from event import Event
 
 class Schedule:
+    dao = ScheduleDAO()
+
     def __init__(self, owner: str, events: Iterable[Event]=[]):
         self.owner = owner
         self.events = set(events)
@@ -13,13 +17,13 @@ class Schedule:
         events = [Event.fromdict(e) for e in sched_dict['events']]
         return cls(owner, events)
 
-    def addEvent(self, event: Event):
+    def add_event(self, event):
         self.events.add(event)
 
-    def removeEvent(self, eventName: str):
-        self.events.remove(eventName)
+    def remove_event(self, event_name):
+        self.events = {e for e in self.events if e.name != event_name}
 
-    def serialize(self):
+        def serialize(self):
         sched_dict = {'owner': self.owner}
         sched_dict['events'] = [e.serialize() for e in self.events]
         return sched_dict
@@ -29,4 +33,21 @@ class Schedule:
             raise KeyError('Schedule must be accessed with a day, received {}'.format(day))
         day_events = (e for e in self.events if day in e.days)
         return sorted(day_events, key=lambda e: e.from_time.in_seconds())
+
+    @classmethod
+    def get(cls, owner):
+        sched_dict = dao.get(owner)
+        if sched_dict is None:
+            abort(404, message='No schedule exists for {}'.format(owner))
+        return cls.fromdict(ched_dict)
+
+    def update(self, event_name, action):
+        if action == 'ADD':
+            event = Event.get(event_name)
+            self.add_event(event)
+        elif action == 'REMOVE':
+            self.remove_event(event_name)
+        else:
+            abort(404, message='Invalid action "{}"'.format(action))
+        return self
 
